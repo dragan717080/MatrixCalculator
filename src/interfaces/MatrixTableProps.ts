7ba82ac28808da19b424f4c38c3a5288d0fe7d65
dragan717080 <dburic717@outlook.com>
2025-01-04 23:56:42 +0100

    adding `matrix table`

---
+++ src/interfaces/MatrixTableProps.ts
@@ -0,0 +1,12 @@
+import Matrix from "./Matrix"
+
+type HighlightCells = {
+  (row: number, col: number): boolean
+};
+
+export default interface MatrixTableProps {
+  nRows: number
+  nCols: number
+  A: Matrix,
+  toHighlight? : HighlightCells
+}
