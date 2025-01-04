4739a6719550ab29d8b132e06933fdbb58366eb8
dragan717080 <dburic717@outlook.com>
2025-01-04 23:56:18 +0100

    adding `matrix table`

---
+++ src/Components/Atoms/MatrixTable.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import MatrixTableProps from '../../interfaces/MatrixTableProps'
+
+const MatrixTable: FC<MatrixTableProps> = ({ nRows, nCols, A, toHighlight }) => {
+  return (
+    <table
+      className='matrix-table mt-5 mb-6 mx-auto overflow-scroll md:overflow-auto text-center'
+    >
+      <thead>
+        <tr>
+          {/* First element is empty */}
+          <th>&nbsp;</th>
+          {Array.from({ length: nRows }).map((_, col) => (
+            <th key={col}>A<span className='subindex'>{col + 1}</span></th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Array.from({ length: nRows }).map((_, row) => (
+          <tr key={row} className='!leading-6.5'>
+            <td className='!text-gray-350'>{row + 1}</td>
+            {Array.from({ length: nCols }).map((_, col) => (
+              <td
+                className={`${toHighlight && toHighlight(row, col) ? 'bg-gray-450 text-neutral-150' : ''} min-h-[2.3125rem] min-w-[2.3125rem] whitespace-nowrap`}
+                key={col}
+              >
+                {A[row][col]}
+              </td>
+            ))}
+          </tr>
+        ))
+        }
+      </tbody>
+    </table>
+  )
+}
+
+export default MatrixTable
