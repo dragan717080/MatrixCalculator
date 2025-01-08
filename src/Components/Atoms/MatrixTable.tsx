9e7a2b77b65d2b23bbd2a90f609825a4f52db3db
dragan717080 <dburic717@outlook.com>
2025-01-08 03:49:14 +0100

    adding `letter` optional param

--- src/Components/Atoms/MatrixTable.tsx
+++ src/Components/Atoms/MatrixTable.tsx
@@ -2,7 +2,7 @@ import React, { FC, forwardRef } from 'react'
 import MatrixTableProps from '../../interfaces/MatrixTableProps'
 
 const MatrixTable = forwardRef<HTMLTableElement, MatrixTableProps>(
-  ({ nRows, nCols, A, toHighlight, className }, ref) => {
+  ({ nRows, nCols, A, toHighlight, className, index, letter }, ref) => {
     const getTableCellValue = (row: number, col: number, value: number) => {
       if (typeof(value) === 'string') {
         value = parseFloat(value)
@@ -14,14 +14,14 @@ const MatrixTable = forwardRef<HTMLTableElement, MatrixTableProps>(
     return (
       <table
         ref={ref}
-        className={`${className} matrix-table mt-5 mb-6 mx-auto overflow-scroll md:overflow-auto text-center`}
+        className={`${className ? className : ''} matrix-table mt-5 mb-6 mx-auto overflow-scroll md:overflow-auto text-center`}
       >
         <thead>
           <tr>
             {/* First element is empty */}
             <th>&nbsp;</th>
             {Array.from({ length: nCols }).map((_, col) => (
-              <th key={col}>A<span className='subindex'>{col + 1}</span></th>
+              <th key={col}>{letter || 'A' }<span className='subindex'>{col + 1}</span></th>
             ))}
           </tr>
         </thead>
@@ -31,7 +31,7 @@ const MatrixTable = forwardRef<HTMLTableElement, MatrixTableProps>(
               <td className='!text-gray-350'>{row + 1}</td>
               {Array.from({ length: nCols }).map((_, col) => (
                 <td
-                  className={`${toHighlight && toHighlight(row, col) ? 'bg-gray-450 text-neutral-150' : ''} min-h-[2.3125rem] min-w-[2.3125rem] whitespace-nowrap`}
+                  className={`${toHighlight && toHighlight(row, col, index) ? 'bg-gray-450 text-neutral-150' : ''} min-h-[2.3125rem] min-w-[2.3125rem] whitespace-nowrap`}
                   key={col}
                 >
                   {getTableCellValue(row, col, A[row][col]!)}
