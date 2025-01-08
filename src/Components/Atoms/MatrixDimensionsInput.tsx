640a1a87bd93cfe32e1dae6ffdfa54b3d56642e5
dragan717080 <dburic717@outlook.com>
2025-01-08 03:44:26 +0100

    adding `isPower` optional param

--- src/Components/Atoms/MatrixDimensionsInput.tsx
+++ src/Components/Atoms/MatrixDimensionsInput.tsx
@@ -1,55 +1,78 @@
-import React, { FC, ChangeEvent, MouseEvent, useRef, useState, useEffect } from 'react'
+import React, { FC, ChangeEvent, MouseEvent, useCallback, useRef, useEffect, useState } from 'react'
 import MatrixDimensionsInputProps from '../../interfaces/MatrixDimensionsInputProps'
 import { TwoNumbers } from '../../interfaces/Matrix'
 import { useMatrixStore, useModalStore } from '../../store/zustandStore'
 
 const MatrixDimensionsInput: FC<MatrixDimensionsInputProps> = ({
   minValue,
-  isSquare = false
+  isSquare = false,
+  isPower = false,
+  isMultiplication = false,
 }) => {
-  const { isOnlyA, aDim, setADim, setA, bDim, setBDim, setB } = useMatrixStore()
+  const { isOnlyA, aDim, setADim, setA, bDim, setBDim, setB, setPower } = useMatrixStore()
   const { isOpen, setIsOpen } = useModalStore()
+
   const aRows = useRef<HTMLInputElement | null>(null)
   const aCols = useRef<HTMLInputElement | null>(null)
   const bRows = useRef<HTMLInputElement | null>(null)
   const bCols = useRef<HTMLInputElement | null>(null)
+  const powerRef = useRef<HTMLInputElement | null>(null)
+  const submitRef = useRef<HTMLButtonElement | null>(null)
 
-  const validateRange = (e: ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value as unknown as number
-    if (value < 1) {
-      e.target.value = '1'
-    } else if (value > 25) {
-      e.target.value = '25'
+  const [allIsFilled, setAllIsFilled] = useState<boolean>(false)
+
+  /** For matrix inputs compare with `minValue` from props, for other take argument in func. */
+  const validateRange = useCallback((
+    e: ChangeEvent<HTMLInputElement>,
+    min: number = minValue,
+    max: number = 25
+  ) => {
+    console.log('in validate range, last char:', e.target.value[e.target.value.length - 1]);
+    if (!Number.isInteger(parseInt(e.target.value[e.target.value.length - 1]))) {
+      console.log('wrote non int');
+      e.target.value = e.target.value.slice(0, -1)
     }
-  }
 
-  const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
-
-  const handleSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    const newAValue = getNumericDimValue()
-
-    // Only opet modal if all dimensions are set
-    const aIsSet = newAValue[0] !== 0 && newAValue[1] !== 0
-    setADim(newAValue)
-
-    if (!isOnlyA) {
-      const newBValue = getNumericDimValue(false)
-      setBDim(newBValue)
-      const bIsSet = newBValue[0] !== 0 && newBValue[1] !== 0
-
-      if (aIsSet && bIsSet) {
-        setIsOpen(true)
-        
-      }
-    } else {
-      console.log('a is set:', aIsSet);
-      if (aIsSet) {
-        console.log('opening modal');
-        setIsOpen(true)
-      }
+    let value = Number(e.target.value); // Directly parse as number
+    if (value < parseFloat(min as unknown as string)) {
+      e.target.value = min as unknown as string;
+    } else if (value > max) {
+      e.target.value = max as unknown as string;
     }
-  }
+
+    const aIsFilled = isSquare
+      ? aRows.current!.value.length
+      : aRows.current!.value.length && aCols.current!.value.length
+
+    console.log('only A:', isOnlyA);
+    const bIsFilled = !isOnlyA
+      ? isSquare
+        ? bRows.current?.value.length || 0
+        : (bRows.current!.value.length || 0) && (bCols.current!.value.length || 0)
+      : true
+
+    const powerIsFilled = isPower
+      ? powerRef.current!.value.length : true
+    console.log(powerIsFilled);
+
+    /** If multiplication is passed, `A` cols must be same as `B` rows. */
+    const multiplicationIsOk = isMultiplication
+      ? aCols.current!.value.length && aCols.current!.value === bRows.current!.value
+      : true
+    const newAllIsFilled = Boolean(aIsFilled && bIsFilled && powerIsFilled && multiplicationIsOk)
+    console.log('a is filled:', aIsFilled, 'b is filled:', bIsFilled, 'power is filled:', powerIsFilled,
+      'multiplication is ok:', multiplicationIsOk
+    );
+    console.log(newAllIsFilled);
+
+    if (newAllIsFilled && powerRef.current) {
+      setPower(parseInt(powerRef.current!.value))
+    }
+
+    setAllIsFilled(newAllIsFilled)
+
+    submitRef.current!.disabled = !newAllIsFilled
+  }, [minValue, isOnlyA])
 
   /** Set `aDim` and `bDim` values, or undefined if they are null. */
   const getNumericDimValue = (isA = true) => {
@@ -65,6 +88,36 @@ const MatrixDimensionsInput: FC<MatrixDimensionsInputProps> = ({
 
     return [bRowsNum, bColsNum] as TwoNumbers
   }
+
+  const handleSubmit = useCallback(
+    async (e: MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      const newAValue = getNumericDimValue();
+
+      // Only open modal if all dimensions are set
+      const aIsSet = newAValue[0] !== 0 && newAValue[1] !== 0;
+      setADim(newAValue);
+
+      if (!isOnlyA) {
+        const newBValue = getNumericDimValue(false);
+        setBDim(newBValue);
+        const bIsSet = newBValue[0] !== 0 && newBValue[1] !== 0;
+
+
+        if (aIsSet && bIsSet) {
+          console.log("opening modal, both matrices are set");
+          setIsOpen(true);
+        }
+      } else {
+        console.log("a is set:", aIsSet);
+        if (aIsSet) {
+          console.log("opening modal");
+          setIsOpen(true);
+        }
+      }
+    },
+    [getNumericDimValue, isOnlyA, setADim, setBDim, setIsOpen] // Dependencies
+  )
 
   useEffect(() => {
     console.log('new A dim in input:', aDim)
@@ -88,6 +141,7 @@ const MatrixDimensionsInput: FC<MatrixDimensionsInputProps> = ({
   useEffect(() => {
     console.log('isOpen in input:', isOpen);
   }, [isOpen])
+
   return (
     <div className='row pt-7'>
       <form action=''>
@@ -97,13 +151,11 @@ const MatrixDimensionsInput: FC<MatrixDimensionsInputProps> = ({
           <input
             required
             ref={aRows}
-            type='number'
-            inputMode='numeric'
+            type='text'
+            inputMode='text'
             onChange={validateRange}
-            min='1'
-            max='25'
             className='p-1.5 pr-0 pb-1.5 pl-2.5 w-[50px] text-sm pointer outline-none rounded-lg focus:bg-primary'
-            value={4}
+            value={2}
           />
           {!isSquare && (
             <>
@@ -111,13 +163,24 @@ const MatrixDimensionsInput: FC<MatrixDimensionsInputProps> = ({
               <input
                 required
                 ref={aCols}
-                type='number'
-                inputMode='numeric'
+                type='text'
+                inputMode='text'
                 onChange={validateRange}
-                min='1'
-                max='25'
                 className='p-1.5 pr-0 pb-1.5 pl-2.5 w-[50px] text-sm pointer outline-none rounded-lg focus:bg-primary'
                 value={2}
+              />
+            </>
+          )}
+          {isPower && (
+            <>
+              <span className='px-2'>Power:</span>
+              <input
+                required
+                ref={powerRef}
+                type='text'
+                inputMode='text'
+                onChange={(e) => validateRange(e, 0, 25)}
+                className='p-1.5 pr-0 pb-1.5 pl-2.5 w-[50px] text-sm pointer outline-none rounded-lg focus:bg-primary'
               />
             </>
           )}
@@ -129,32 +192,30 @@ const MatrixDimensionsInput: FC<MatrixDimensionsInputProps> = ({
             <input
               required
               ref={bRows}
-              type='number'
-              inputMode='numeric'
+              type='text'
+              inputMode='text'
               onChange={validateRange}
-              min='1'
-              max='25'
               className='p-1.5 pr-0 pb-1.5 pl-2.5 w-[50px] text-sm pointer outline-none rounded-lg focus:bg-primary'
-            /*               value={2} */
+                           value={2} 
             />
             <span className='px-2'>X</span>
             <input
               required
               ref={bCols}
-              type='number'
-              inputMode='numeric'
+              type='text'
+              inputMode='text'
               onChange={validateRange}
-              min='1'
-              max='25'
               className='p-1.5 pr-0 pb-1.5 pl-2.5 w-[50px] text-sm pointer outline-none rounded-lg focus:bg-primary'
-            /*               value={4} */
+                           value={2} 
             />
           </div>
         )}
         <button
-          type='submit'
+          ref={submitRef}
+/*           disabled={!allIsFilled}
+ */          type='submit'
           onClick={(e) => handleSubmit(e)}
-          className='row-h rounded-md mt-7 mb-5 lg:mt-10 lg:mb-8 mx-auto px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-sky-500 hover:bg-sky-600 text-gray-300 focus-visible:outline-sky-600'
+          className='row-h disabled:opacity-50 disabled:pointer-none rounded-md mt-7 mb-5 lg:mt-10 lg:mb-8 mx-auto px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-sky-500 hover:bg-sky-600 text-gray-300 focus-visible:outline-sky-600'
         >
           Set {!isOnlyA ? 'matrix' : 'matrices'}
         </button>
